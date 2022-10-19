8096b515c8eb72da93ea36f2f7403049260ed3df
pc20211139 <115052276+pc20211139@users.noreply.github.com>
2022-10-19 09:41:38 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,30 @@
 let x_value = 0
 let y = 0
 input.onButtonPressed(Button.A, function () {
-    x_value = 0
-    for (let index = 0; index < 5; index++) {
-        led.plot(x_value, 0)
-        basic.pause(1000)
-        basic.clearScreen()
-        x_value += 1
-        led.plot(x_value, 0)
-        basic.pause(1000)
-        basic.clearScreen()
-    }
+	
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 4; index++) {
-        y = 0
-        led.plot(0, y)
-        basic.pause(1000)
-        basic.clearScreen()
-        y += 1
-        led.plot(0, y)
-        basic.pause(1000)
-        basic.clearScreen()
-    }
+	
 })
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        led.plot(x_value, 0)
+        basic.pause(1000)
+        x_value += 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        led.plot(0, y)
+        basic.pause(1000)
+        y += 1
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.clearScreen()
+        led.plot(x_value, 0)
+        basic.pause(1000)
+        x_value += -1
+        basic.clearScreen()
+        x_value += 1
+    }
 })
